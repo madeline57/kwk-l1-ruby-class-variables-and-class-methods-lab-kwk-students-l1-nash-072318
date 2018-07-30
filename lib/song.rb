e68a16ea5a81e8d98fb8bea559cbe8765999ca5c
madeline57 <maddiejobeth@aol.com>
2018-07-30 14:57:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,43 @@
+class Song 
+  def  initialize(song_name,artist,genre)
+    @song_name = song_name
+    @artist = artist 
+    @genre = genre
+   @@counter += 1 
+  end 
+  
+  def song_name = (new_song_name)
+    @song_name = new_song_name
+  end 
+  
+  def artist
+    @artist
+  end 
+
+  def genre = (new_genre)
+    @genre = new_genre
+  end 
+attr_accessor :song_name, :artist, :genre,
+  def self.counter
+     @@counter
+  end
+end
+ @@counter = 0 
+ 
+ 
+ 
+ 
+ we_dont_have_to_dance = ("we dont have to dance", "andy black", "inde") 
+ 
+
+
+
+
+
+
+
+
+
+
+
 
